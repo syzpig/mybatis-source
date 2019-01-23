ed3581c13f0d9557ce2647063e3054e2739e20c2
shaoyingzhu <yingzhu.shao@newtouch.cn>
2019-01-23 18:25:19 +0800

    初始化mybatis源码测试demo等案例

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# mybatis-source
-模拟mybatis源码实现1.0,-2.0
+#gupaoedu-mybatis
---
+++ src/main/java/com/study/mybatis/demo/Demo.java
@@ -0,0 +1,32 @@
+package com.study.mybatis.demo;
+
+import com.study.mybatis.beans.Test;
+import com.study.mybatis.mapper.TestMapper;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+/**
+ * Created by  on 2017-09-08.
+ *
+ *
+ */
+public class Demo {
+    public static SqlSession getSqlSession() throws FileNotFoundException {
+        //配置文件
+        InputStream configFile = new FileInputStream(
+                "D:\\GuPaoPorxy\\gupaoedu-mybatis\\src\\main\\java\\com\\gupaoedu\\mybatis\\demo\\mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(configFile);
+        //加载配置文件得到SqlSessionFactory
+        return sqlSessionFactory.openSession();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        TestMapper testMapper = getSqlSession().getMapper(TestMapper.class);
+        Test test = testMapper.selectByPrimaryKey(1);
+    }
+}
